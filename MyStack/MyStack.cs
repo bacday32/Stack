b42d7c1d769296e3260dcb9365e6f2ab66a61075
BacDay32 <lephuocbac@gmail.com>
2020-12-02 18:50:31 +0700

    first commit

---
+++ MyStack/MyStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    class MyStack<T>
+    {
+        private int topIndex { get; set; } = -1;
+        private List<T> lst { get; set; } = new List<T>();        
+        private T item { get; set; } = default(T);
+        public MyStack(int indexStack, List<T> lst)
+        {
+            this.topIndex = -1;
+            this.lst = new List<T>();
+        }
+        public MyStack()
+        {
+
+        }
+
+        public void Push(T valueStack)
+        {
+            //lst[topIndex] = valueStack;          
+            lst.Add(valueStack);
+            topIndex++;
+        }
+        public T Pop()
+        {
+            T item = lst[topIndex];
+            lst.RemoveAt(topIndex);
+            topIndex--;
+            return item;
+        }
+
+        public T Get()
+        {
+            return lst[topIndex];
+        }
+       
+        public bool IsEmpty()
+        {
+            return lst.Count == 0;
+        }
+        public int Count()
+        {
+            return lst.Count();
+        }
+
+    }
+}
---
+++ MyStack/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create MyStack type string
+            MyStack<string> st = new MyStack<string>();
+
+
+            //Console.Write("enter length stack :");
+            //get length stack
+            //int lengthStack = int.Parse(Console.ReadLine());
+                     
+            Console.Write("how many values do you want enter the stack  :");
+            //get number value stack
+            int numberValueStack = int.Parse(Console.ReadLine());
+          
+
+
+            for (int i = 0; i < numberValueStack; i++)
+            {
+                Console.Write("enter values in stack :");
+                //enter value 
+                string valueStack = Console.ReadLine();
+                //push value 
+                st.Push(valueStack);
+            }
+            Console.Write("Stack is full :");
+            
+                       
+            Console.Write("Do you want push/pop?? :");
+            string answer = Console.ReadLine();
+            if(answer=="pop")
+            {   
+
+                Console.WriteLine("how many values do you wan pop the stack : ");
+
+            }
+            else if(answer=="push")
+            {
+
+            }
+
+
+
+
+
+
+
+
+            /*if (answer == "pop")
+            {   
+                Console.WriteLine(st.Pop());
+                Console.ReadKey();
+            }
+            else if(answer == "push")
+            {
+                Console.Write("enter values in stack :");
+                string valueStack = Console.ReadLine();
+                st.Push(valueStack);
+            }*/
+
+
+
+
+
+
+            //Console.WriteLine(st.Pop());
+            //Console.WriteLine(valueStack);                                                                            
+
+
+            /*Console.WriteLine(st.Pop());
+            Console.WriteLine(st.Pop());
+            Console.WriteLine(st.Pop());
+            Console.WriteLine("pop");*/
+
+        }
+    }
+}
